7635dc55ab2836c89970dc6bc235bb8e1bed370a
Srinivas Narayana <ngsrinivas@gmail.com>
2020-07-14 11:52:39 -0400

    Add templated generation of conference/workshop programs

--- gen.py
+++ gen.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python
 
+import subprocess
+
 header_file = 'header'
 footer_file = 'footer'
 data_folder = 'data-files/'
 stage_folder = 'staging/sigcomm/2020/'
+gen_script = 'gen.sh'
 
 # Add list of files to generate here
 data_files = ['index',
@@ -31,6 +34,8 @@ data_files = ['index',
               'tutorial-quic',
               'n2women']
 
+templated_files = {'program': 'main-program'}
+
 def read_file(fname):
     with open(data_folder + fname + '.html', 'r') as f:
         data = f.read()
@@ -44,5 +49,14 @@ header = read_file(header_file)
 footer = read_file(footer_file)
 
 for f in data_files:
+    data = read_file(f)
+    write_file(f, header + data + footer)
+
+for f in templated_files.keys():
+    script_folder = templated_files[f]
+    cmd = "cd %s%s ; sh %s ; cd -" % (
+        data_folder, script_folder, gen_script)
+    print cmd
+    subprocess.call(cmd, shell=True)
     data = read_file(f)
     write_file(f, header + data + footer)
